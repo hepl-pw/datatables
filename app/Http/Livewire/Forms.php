173ee380d53747525b0290d1ce1a82594adfb407
vagrant <vagrant@homestead>
2020-10-30 16:22:15 +0000

    extract forms to livewire component

--- app/Http/Livewire/Forms.php
+++ app/Http/Livewire/Forms.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class Forms extends Component
 {
+    public $qp;
+    public $perPage;
+    public $searchTerm;
+
     public function render()
     {
         return view('livewire.forms');
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,48 +16,11 @@
 <body>
 <main class="container">
     <h1>Mes Contacts</h1>
-    <div class="row">
-        <form class="col" action="/">
-            <div class="form-group">
-                <label for="per-page">Per Page:</label>
-                <select name="per-page"
-                        class="form-control"
-                        id="per-page">
-                    <option value="10" {{$perPage == 10 ? 'selected' : ''}}>10</option>
-                    <option value="15" {{$perPage == 15 ? 'selected' : ''}}>15</option>
-                    <option value="25" {{$perPage == 25 ? 'selected' : ''}}>25</option>
-                </select>
-            </div>
-            @foreach($qp as $k => $p)
-                @if($k!='per-page')
-                    <input type="hidden" name="{{$k}}" value="{{$p}}">
-                @endif
-            @endforeach
-            <noscript>
-                <button type="submit">Change pagination</button>
-            </noscript>
-        </form>
-        <form class="col" action="/">
-            <div class="form-group">
-                <label for="search-term">Search term or email:</label>
-                <input type="search"
-                       class="form-control"
-                       name="search-term"
-                       id="search-term"
-                       value="{{$searchTerm}}"
-                       placeholder="name or email"
-                >
-            </div>
-            @foreach($qp as $k => $p)
-                @if($k!='search-term')
-                    <input type="hidden" name="{{$k}}" value="{{$p}}">
-                @endif
-            @endforeach
-            <noscript>
-                <button type="submit">Search</button>
-            </noscript>
-        </form>
-    </div>
+    @livewire('forms',[
+    'qp' => $qp,
+    'perPage' => $perPage,
+    'searchTerm' => $searchTerm
+    ])
     <hr>
     @livewire('data-table',[
     'qp' => $qp,
